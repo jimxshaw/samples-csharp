2f4cf8c921140c6cef7672bdc85da18f9fb2d99f
Jim <jimxshaw@gmail.com>
2015-08-18 16:38:52 -0500

    multiplication table

---
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t1\t2\t3\t4\t5\t6\t7\t8\t9");
+
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.Write(i);
+
+                for (int j = 1; j <= 9; j++)
+                {
+                    Console.Write("\t{0}", i * j);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
