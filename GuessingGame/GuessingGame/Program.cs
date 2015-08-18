9c5889ff4eabae652ed0731fb6e6d03a41b13685
Jim <jimxshaw@gmail.com>
2015-08-18 16:08:04 -0500

    refactored

--- GuessingGame/GuessingGame/Program.cs
+++ GuessingGame/GuessingGame/Program.cs
@@ -18,12 +18,20 @@ namespace GuessingGame
             Random rng = new Random();
             theAnswer = rng.Next(1, 21);
 
+            Console.WriteLine("The answer is greater than 0 but less than 21.");
+
             do
             {
                 Console.Write("Enter your guess: ");
                 playerInput = Console.ReadLine();
 
                 playerGuess = int.Parse(playerInput);
+
+                if (playerGuess < 1 || playerGuess > 20)
+                {
+                    Console.WriteLine("Your guess is out of range. Try again.");
+                    continue;
+                }
 
                 if (playerGuess == theAnswer)
                 {
@@ -34,11 +42,11 @@ namespace GuessingGame
                 {
                     if (playerGuess > theAnswer)
                     {
-                        Console.WriteLine("Too high");
+                        Console.WriteLine("Too high!");
                     }
                     else
                     {
-                        Console.WriteLine("Too low");
+                        Console.WriteLine("Too low!");
                     }
                 }
             }
