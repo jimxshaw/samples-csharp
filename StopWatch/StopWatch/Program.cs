1dd6cc9b42e537ed78ed82c5b3ec9739bcc6c9f2
Jim <jimxshaw@gmail.com>
2015-08-20 20:50:46 -0500

    added stop watch

---
+++ StopWatch/StopWatch/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Press enter to star the timer.");
+            Console.ReadLine();
+
+            StopWatch myStopWatch = new StopWatch();
+            myStopWatch.Start();
+
+            Console.Write("Press enter to stop the timer.");
+            Console.ReadLine();
+            myStopWatch.Stop();
+
+            Console.ReadLine();
+        }
+    }
+}
