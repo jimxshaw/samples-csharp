50e24a35a8f9d1bbba3811d2d8f8768544439a71
Jim <jimxshaw@gmail.com>
2015-08-20 21:59:13 -0500

    constructors

---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            Console.WriteLine("Area: {0}", r.GetArea());
+
+            Rectangle r2 = new Rectangle(12, 18);
+            Console.WriteLine("Area: {0}", r2.GetArea());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Constructors/Constructors/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Rectangle
+    {
+        private int length;
+        private int width;
+
+        public Rectangle()
+        {
+            Console.WriteLine("We are in the rectangle's constructor");
+            length = 0;
+            width = 0;
+        }
+
+        public Rectangle(int sideA, int sideB)
+        {
+            Console.WriteLine("This is the constructor that takes two values");
+            length = sideA;
+            width = sideB;
+        }
+
+        public int GetArea()
+        {
+            return length * width;
+        }
+    }
+}
