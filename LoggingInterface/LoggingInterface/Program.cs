6392d37150293e94bb4313d4679e81548320e87b
Jim <jimxshaw@gmail.com>
2015-08-25 19:32:26 -0500

    interface sample

---
+++ LoggingInterface/LoggingInterface/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggingInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger log = ILoggerFactory.GetLogger();
+
+            log.WriteToLog("Hello world!");
+
+            Console.ReadLine();
+        }
+    }
+}
