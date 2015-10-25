a5e157fb0864f82dbdec047a250276ea07d8203f
Jim Shaw <jimxshaw@gmail.com>
2015-10-25 18:04:45 -0400

    modified views

--- samples-aspnet/SimpleBlogMVC/SimpleBlogMVC/Controllers/AuthController.cs
+++ samples-aspnet/SimpleBlogMVC/SimpleBlogMVC/Controllers/AuthController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SimpleBlogMVC.ViewModels;
 
 namespace SimpleBlogMVC.Controllers
 {
     public class AuthController : Controller
     {
+        // Get action is by default
         public ActionResult Login()
         {
-            return View();
+            return View(new AuthLogin
+            {
+                // Test = "This is the test value set in my controller!"
+            });
             //return Content("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            // form.Test = "This is the value set in my post action";
+            return View(form);
+            // return Content("Hey " + form.Username + ", how are you?");
         }
     }
 }
---
+++ samples-aspnet/SimpleBlogMVC/SimpleBlogMVC/ViewModels/Auth.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleBlogMVC.ViewModels
+{
+    public class AuthLogin
+    {
+        // public string Test { get; set; }
+
+        public string Username { get; set; }
+
+        // Data
+        public string Password { get; set; }
+    }
+}
