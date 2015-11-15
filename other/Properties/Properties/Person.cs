f685443bb1ae4909132e4269ef72a496619f1279
Jim Shaw <jimxshaw@gmail.com>
2015-11-14 21:05:45 -0500

    properties sample

---
+++ other/Properties/Properties/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Properties
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        // A private set access means it is readonly. It cannot
+        // be changed. It must be set in the constructor.
+        public DateTime Birthdate { get; private set; }
+
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
+
+                return years;
+            }
+        }
+    }
+}
