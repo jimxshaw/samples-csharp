642fab53aa48bb4749dfd32bafa3af2633bbfb4a
Jim Shaw <jimxshaw@gmail.com>
2015-11-23 15:33:06 -0500

    consolidated models

---
+++ samples-aspnet/BookEntity/BookEntity/Models/ModelGroup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookEntity.Models
+{
+    public class ModelGroup
+    {
+        public class Book
+        {
+            public int BookID { get; set; }
+
+            public string BookName { get; set; }
+
+            public string ISBN { get; set; }
+        }
+
+        public class Review
+        {
+            public int ReviewID { get; set; }
+
+            public int BookID { get; set; }
+
+            public string ReviewText { get; set; }
+        }
+    }
+}
