ada5bad1bf3317dfc720e0e0d9d5fcf2c458a692
Jim Shaw <jimxshaw@gmail.com>
2015-11-23 15:53:42 -0500

    added dbcontext and connectionstring

---
+++ samples-aspnet/BookEntity/BookEntity/BooksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BookEntity
+{
+    public class BooksDbContext : DbContext
+    {
+        // This context class can perform CRUD operations on the Book and 
+        // Review models since it has the DbSet defined for both of them. 
+        public DbSet<Models.ModelGroup.Book> Books { get; set; }
+        public DbSet<Models.ModelGroup.Review> Reviews { get; set; }
+    }
+}
--- samples-aspnet/BookEntity/BookEntity/Models/ModelGroup.cs
+++ samples-aspnet/BookEntity/BookEntity/Models/ModelGroup.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,22 +9,33 @@ namespace BookEntity.Models
 {
     public class ModelGroup
     {
+        [Table("Books")] // Table name
         public class Book
         {
+            [Key] // Primary key
             public int BookID { get; set; }
 
             public string BookName { get; set; }
 
             public string ISBN { get; set; }
+
+            // This is to maintain the many reviews associated with a book entity
+            public virtual ICollection<Review> Review { get; set; } 
         }
 
+        [Table("Reviews")] // Table name
         public class Review
         {
+            [Key]
             public int ReviewID { get; set; }
 
+            [ForeignKey("Book")]
             public int BookID { get; set; }
 
             public string ReviewText { get; set; }
+
+            // This will keep track of the book of which this particular review belongs
+            public virtual Book Book { get; set; }
         }
     }
 }
