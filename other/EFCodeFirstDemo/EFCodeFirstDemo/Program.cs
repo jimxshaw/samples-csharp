a995cace600eccf9f1db1d2a8012c1206fb1ea27
Jim Shaw <jimxshaw@gmail.com>
2015-11-23 22:53:05 -0500

    added EFCodeFirstDemo

---
+++ other/EFCodeFirstDemo/EFCodeFirstDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCodeFirstDemo
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public DateTime DatePublished { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+
+    // Db Context class is an abstraction over the database that
+    // is used to load or save data.
+    public class BlogDbContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- other/EFDatabaseFirstDemo/EFDatabaseFirstDemo/Program.cs
+++ other/EFDatabaseFirstDemo/EFDatabaseFirstDemo/Program.cs
@@ -10,6 +10,18 @@ namespace EFDatabaseFirstDemo
     {
         static void Main(string[] args)
         {
+            var context = new DatabaseFirstDemoEntities();
+            var post = new Post()
+            {
+                PostID = 1,
+                Title = "Entity Framework Database First Example",
+                DatePublished = DateTime.Now,
+                Body = "This is a sample app that demonstrates Entity Framework with the Database First approach."
+            };
+
+            context.Posts.Add(post);
+
+            context.SaveChanges();
         }
     }
 }
