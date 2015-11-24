c629e9b19b05c9d1a3d51fe3825e7e274b2e87ef
Jim Shaw <jimxshaw@gmail.com>
2015-11-23 23:39:51 -0500

    added indexer example

---
+++ other/Indexers/Indexers/HttpCookie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Indexers
+{
+    public class HttpCookie
+    {
+        private readonly Dictionary<string, string> _dictionary;
+        public DateTime Expiry { get; set; }
+
+        public HttpCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+
+        // Indexers have no names. They must utilize this
+        // naming convention.
+        public string this[string key]
+        {
+            get { return _dictionary[key]; }
+            set { _dictionary[key] = value; }
+        }
+
+        // Without indexers, the below methods would have to
+        // be created. There's nothing wrong with using them.
+        // Indexers are simply more efficient.
+        /*
+            public void SetItem(string key, string value)
+            {
+            
+            }
+
+            public void GetItem(string key)
+            {
+            
+            }
+        */
+    }
+}
---
+++ other/Indexers/Indexers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HttpCookie();
+
+            // name is the dicionary key, James is 
+            // the dictionary value.
+            cookie["name"] = "James";
+
+            Console.WriteLine(cookie["name"]);
+
+            Console.ReadLine();
+        }
+    }
+}
