ae0def1dba6effaeb5bafe27e633b248825c61af
Jim Shaw <jimxshaw@gmail.com>
2015-11-26 13:39:42 -0500

    imported stored procedures into db context

--- other/EFDbFirst/EFDbFirst/Program.cs
+++ other/EFDbFirst/EFDbFirst/Program.cs
@@ -10,6 +10,16 @@ namespace EFDbFirst
     {
         static void Main(string[] args)
         {
+            var dbContext = new PlutoDbContext();
+            var courses = dbContext.GetCourses();
+
+            foreach (var c in courses)
+            {
+                Console.WriteLine(c.Title);
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
