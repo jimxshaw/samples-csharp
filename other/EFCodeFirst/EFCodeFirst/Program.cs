732a501f6ba8f3b141dbb6db60c1447437e70ff1
Jim Shaw <jimxshaw@gmail.com>
2015-11-28 15:29:40 -0500

    EF code first, created a new db

---
+++ other/EFCodeFirst/EFCodeFirst/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations.Model;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCodeFirst
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public CourseLevel Level { get; set; }
+        public float FullPrice { get; set; }
+        public Author Author { get; set; }
+        public IList<Tag> Tags { get; set; } 
+    }
+
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Course> Courses { get; set; }
+    }
+
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Course> Courses { get; set; } 
+    }
+
+    public enum CourseLevel
+    {
+        Beginner = 1,
+        Intermediate = 2,
+        Advanced = 3
+    }
+
+    public class PlutoContext : DbContext
+    {
+        // Db sets are collections of objects that represent
+        // tables in a database.
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        public PlutoContext() : base("name=DefaultConnection")
+        {
+            
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
