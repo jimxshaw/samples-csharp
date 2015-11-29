2c1929103c3e268d9e1265c1581c7a396db4211b
Jim Shaw <jimxshaw@gmail.com>
2015-11-28 23:16:40 -0500

    modified seed method

--- other/EFCodeFirstExistingDb/EFCodeFirstExistingDb/Migrations/Configuration.cs
+++ other/EFCodeFirstExistingDb/EFCodeFirstExistingDb/Migrations/Configuration.cs
@@ -1,3 +1,5 @@
+using System.Collections.ObjectModel;
+
 namespace EFCodeFirstExistingDb.Migrations
 {
     using System;
@@ -26,6 +28,16 @@ namespace EFCodeFirstExistingDb.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            context.Authors.AddOrUpdate(a => a.Name, 
+                new Author()
+                {
+                    Name = "Author 1",
+                    Courses = new Collection<Course>()
+                    {
+                        new Course() {Name = "Course for Author 1", Description = "Course Description" }
+                    }
+                });
         }
     }
 }
