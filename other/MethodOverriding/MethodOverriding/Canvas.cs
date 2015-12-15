afeb4dbebba9b3580ee592ccd83433185ed51813
Jim Shaw <jimxshaw@gmail.com>
2015-12-15 17:10:35 -0600

    initial commit

---
+++ other/MethodOverriding/MethodOverriding/Canvas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverriding
+{
+    public class Canvas
+    {
+        public void DrawShapes(List<Shape> shapes)
+        {
+            foreach (var shape in shapes)
+            {
+                switch (shape.Type)
+                {
+                    case ShapeType.Circle:
+                        Console.WriteLine("Draw a circle");
+                        break;
+                    case ShapeType.Square:
+                        Console.WriteLine("Draw a square");
+                        break;
+                    case ShapeType.Hexagon:
+                        Console.WriteLine("Draw a hexagon");
+                        break;
+                }
+            }
+        }
+    }
+}
