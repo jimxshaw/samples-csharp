3d57a6cfbba7ee72f7d2534f4238d9c677803afe
Jim Shaw <jimxshaw@gmail.com>
2015-12-20 11:18:18 -0600

    added ContactVM.cs, implemented Contact view

---
+++ samples-aspnet/TheWorld/src/TheWorld/ViewModels/ContactViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TheWorld.ViewModels
+{
+    public class ContactViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
