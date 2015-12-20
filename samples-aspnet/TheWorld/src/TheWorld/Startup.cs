296823a830b315d98592c9b75d3878e90ee5232f
Jim Shaw <jimxshaw@gmail.com>
2015-12-20 12:04:27 -0600

    added IMailService interface, DebugMailService.cs, modified startup.cs to take in the new service

--- samples-aspnet/TheWorld/src/TheWorld/Startup.cs
+++ samples-aspnet/TheWorld/src/TheWorld/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
+using TheWorld.Services;
 
 namespace TheWorld
 {
@@ -16,6 +17,11 @@ namespace TheWorld
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            // By creating the scoped service, it allows the AppController to get an
+            // instance of the mail service through DebugMailService (which implements
+            // the IMailService interface).
+            services.AddScoped<IMailService, DebugMailService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
