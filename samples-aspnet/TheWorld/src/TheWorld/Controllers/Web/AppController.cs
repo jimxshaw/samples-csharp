084265ac9e423f40ad5d7cf7b680454f1d5c157b
Jim Shaw <jimxshaw@gmail.com>
2015-12-20 12:38:29 -0600

    added validation to Contact HttpPost action

--- samples-aspnet/TheWorld/src/TheWorld/Controllers/Web/AppController.cs
+++ samples-aspnet/TheWorld/src/TheWorld/Controllers/Web/AppController.cs
@@ -32,10 +32,30 @@ namespace TheWorld.Controllers.Web
         [HttpPost]
         public IActionResult Contact(ContactViewModel model)
         {
-            _mailService.SendMail("",
-                "",
-                $"Contact Page from {model.Name} ({model.Email})",
-                model.Message);
+            // Despite model validation on the view, validation within the action is
+            // still necessary. When the form is submitted and this data is posted to us
+            // it re-confirms that it's still valid so that bad data wouldn't be saved to
+            // the database, for example.
+            if (ModelState.IsValid)
+            {
+                var email = Startup.Configuration["AppSettings:SiteEmailAddress"];
+
+                if (string.IsNullOrWhiteSpace(email))
+                {
+                    ModelState.AddModelError("", "Could not send email, configuration issue");
+                }
+
+                if (_mailService.SendMail(email, // to
+                    email, // from
+                    $"Contact Page from {model.Name} ({model.Email})", // submit
+                    model.Message)) // body
+                {
+                    // If the form is submitted then clear the form.
+                    ModelState.Clear();
+
+                    ViewBag.Message = "Mail sent. Thanks!";
+                }
+            }
 
             return View();
         }
--- samples-aspnet/TheWorld/src/TheWorld/Startup.cs
+++ samples-aspnet/TheWorld/src/TheWorld/Startup.cs
@@ -5,13 +5,31 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TheWorld.Services;
+using Microsoft.Extensions.PlatformAbstractions;
 
 namespace TheWorld
 {
     public class Startup
     {
+        public static IConfigurationRoot Configuration;
+
+        public Startup(IApplicationEnvironment appEnv)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(appEnv.ApplicationBasePath)
+                // Set the base path first so that our app will know where 
+                // to find the below config.json file.
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+
+            // This will take the configuration info from the environment variables and
+            // give us a dictionary of name-value pairs that we can use. 
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
