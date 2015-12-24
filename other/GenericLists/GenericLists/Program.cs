8e927de70ea2ad5ffa813bd7f494057c7c659b7f
Jim Shaw <jimxshaw@gmail.com>
2015-12-24 15:57:10 -0600

    added bad code to test the sample app

--- other/GenericLists/GenericLists/Program.cs
+++ other/GenericLists/GenericLists/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,12 +32,27 @@ namespace GenericLists
             Console.WriteLine("Count = " + presidents.Count);
             Console.WriteLine("Capacity = " + presidents.Capacity + "\r\n");
 
+            // var copy = presidents.AsReadOnly();
+            // The above line does the exact same thing as
+            // the below line. In fact, the below line is
+            // what's occuring under the hood.
+            var copy = new ReadOnlyCollection<string>(presidents);
+
+
+            BadCode(copy);
+
+
             foreach (var president in presidents)
             {
                 Console.WriteLine(president);
             }
 
             Console.ReadLine();
+        }
+
+        static void BadCode(IList<string> lst)
+        {
+            lst.RemoveAt(2);
         }
     }
 }
