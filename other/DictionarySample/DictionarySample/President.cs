c645331162a7bbceb8f5c5e64533f83321068e35
Jim Shaw <jimxshaw@gmail.com>
2015-12-26 13:44:49 -0600

    added DictionarySample

---
+++ other/DictionarySample/DictionarySample/President.cs
@@ -0,0 +1,19 @@
+﻿namespace DictionarySample
+{
+    public class President
+    {
+        public string Name { get; set; }
+        public int YearElected { get; set; }
+
+        public President(string name, int yearElected)
+        {
+            Name = name;
+            YearElected = yearElected;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}, elected: {1}", Name, YearElected);
+        }
+    }
+}
---
+++ other/DictionarySample/DictionarySample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionarySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var presidents = new Dictionary<string, President>()
+            {
+                {"AL", new President("Abraham Lincoln", 1860)},
+                {"WW", new President("Woodrow Wilson", 1912)},
+                {"FDR", new President("Franklin Roosevelt", 1932)}
+            };
+
+            foreach (var president in presidents)
+            {
+                Console.WriteLine(president);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
