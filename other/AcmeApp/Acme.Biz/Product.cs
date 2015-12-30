c824def4ed151fbcac5f5c65c7bdfe3166074d89
Jim Shaw <jimxshaw@gmail.com>
2015-12-30 13:54:26 -0600

    added Product.cs and Unit Test for it

---
+++ other/AcmeApp/Acme.Biz/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    /// <summary>
+    /// Manages products carried in inventory.
+    /// </summary>
+    public class Product
+    {
+        private int productId;
+        private string productName;
+        private string description;
+
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+
+        public string SayHello()
+        {
+            return "Hello " + 
+                ProductName + 
+                " (" + ProductId + "): " + 
+                Description;
+        }
+
+    }
+}
---
+++ other/AcmeApp/Tests/Acme.BizTests/ProductTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+        [TestMethod()]
+        public void SayHelloTest()
+        {
+            //Arrange
+            var currentProduct = new Product();
+            currentProduct.ProductId = 1;
+            currentProduct.ProductName = "Hammer";
+            currentProduct.Description = "A sturdy tool";
+
+            var expected = "Hello Hammer (1): A sturdy tool";
+
+            //Act
+            var actual = currentProduct.SayHello();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
