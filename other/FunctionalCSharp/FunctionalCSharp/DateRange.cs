3fff07192eabc195bc7a620ff772b22198a9aed8
Jim Shaw <jimxshaw@gmail.com>
2016-01-01 12:43:18 -0600

    added FunctionalCSharp sample

---
+++ other/FunctionalCSharp/FunctionalCSharp/DateRange.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FunctionalCSharp
+{
+    public class DateRange
+    {
+        // To enforce immutability, make the setters private and
+        // then add a constructor.
+
+        public DateTime Start { get; private set; }
+        public DateTime End { get; private set; }
+
+        public DateRange(DateTime start, DateTime end)
+        {
+            if (start.CompareTo(end) >= 0) throw new Exception("End date must occur after start date");
+
+            Start = start;
+            End = end;
+        }
+
+        public bool DateIsInRange(DateTime checkDate)
+        {
+            // True or False: the start date is prior to the end date AND the end
+            // date is past the start end.
+            return Start.CompareTo(checkDate) <= 0 && End.CompareTo(checkDate) >= 0;
+        }  
+    }
+}
---
+++ other/FunctionalCSharp/FunctionalCSharp/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testDates = new List<DateTime>
+            {
+                DateTime.Parse("2015-01-03"),
+                DateTime.Parse("2015-04-12"),
+                DateTime.Parse("2015-06-20"),
+                DateTime.Parse("2015-10-31"),
+                DateTime.Parse("2015-12-24")
+            };
+
+            // PART 1:
+            //var range = new DateRange { Start = DateTime.Parse("2015-05-31"), End = DateTime.Parse("2015-11-30") };
+
+            //testDates.ForEach(d =>
+            //{
+            //    var myDates = $"{d:yyyy-MM-dd} - {(range.DateIsInRange(d))}";
+            //    Console.WriteLine(myDates);
+            //});
+
+            //range.End = DateTime.MaxValue;
+
+            //Console.WriteLine("The End property has changed so therefore the LINQ query results changed.");
+            //Console.WriteLine("This means that the DateRange type is mutable. To follow the functional");
+            //Console.WriteLine("programming paradigm, the type should be immutable.");
+
+            //testDates.ForEach(d =>
+            //{
+            //    var myDates = $"{d:yyyy-MM-dd} - {(range.DateIsInRange(d))}";
+            //    Console.WriteLine(myDates);
+            //});
+
+
+            // PART 2:
+            var range = new DateRange(DateTime.Parse("2015-05-31"), DateTime.Parse("2015-11-30"));
+
+            testDates.ForEach(d =>
+            {
+                var myDates = $"{d:yyyy-MM-dd} - {(range.DateIsInRange(d))}";
+                Console.WriteLine(myDates);
+            });
+
+            // The Main() method still wants to change the range's end date.
+            // So we can create a new date range and use it instead.
+            // By creating a new DateRange instance, we more clearly expressed
+            // that range and range2 represent different date ranges and we've done
+            // it without altering the original values. 
+
+            var range2 = new DateRange(range.Start, DateTime.MaxValue);
+
+            testDates.ForEach(d =>
+            {
+                var myDates = $"{d:yyyy-MM-dd} - {(range.DateIsInRange(d))}";
+                Console.WriteLine(myDates);
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
