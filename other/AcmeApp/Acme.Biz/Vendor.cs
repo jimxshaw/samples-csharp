943b089223f9181bceda6e4ebeaf9836dbb8664d
Jim Shaw <jimxshaw@gmail.com>
2016-01-04 23:04:52 -0600

    added PlaceOrder method in Vendor class and unit tests

--- other/AcmeApp/Acme.Biz/Vendor.cs
+++ other/AcmeApp/Acme.Biz/Vendor.cs
@@ -10,11 +10,41 @@ namespace Acme.Biz
     /// <summary>
     /// Manages the vendors from whom we purchase our inventory.
     /// </summary>
-    public class Vendor 
+    public class Vendor
     {
         public int VendorId { get; set; }
         public string CompanyName { get; set; }
         public string Email { get; set; }
+
+        /// <summary>
+        /// Sends a product order to the vendor.
+        /// </summary>
+        /// <param name="product">Product to order.</param>
+        /// <param name="quantity">Quantity of the product to order.</param>
+        /// <returns></returns>
+        public OperationResult PlaceOrder(Product product, int quantity)
+        {
+            // Guard clauses make sure passed in values are within constraints.
+            if (product == null) throw new ArgumentNullException(nameof(product));
+            if (quantity <= 0) throw new ArgumentOutOfRangeException(nameof(product));
+
+            var success = false;
+
+            var orderText = "Order from ACME, Inc" + "\n" +
+                            "Product: " + product.ProductCode + "\n" +
+                            "Quantity: " + quantity;
+
+            var emailService = new EmailService();
+            var confirmation = emailService.SendMessage("New Order", orderText, Email);
+
+            if (confirmation.StartsWith("Message sent: "))
+            {
+                success = true;
+            }
+
+            var operationResult = new OperationResult(success, orderText);
+            return operationResult;
+        }
 
         /// <summary>
         /// Sends an email to welcome a new vendor.
@@ -25,7 +55,7 @@ namespace Acme.Biz
             var emailService = new EmailService();
             var subject = ("Hello " + this.CompanyName).Trim();
             var confirmation = emailService.SendMessage(subject,
-                                                        message, 
+                                                        message,
                                                         this.Email);
             return confirmation;
         }
--- other/AcmeApp/Tests/Acme.BizTests/VendorTests.cs
+++ other/AcmeApp/Tests/Acme.BizTests/VendorTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Acme.Common;
 
 namespace Acme.Biz.Tests
 {
@@ -54,6 +55,48 @@ namespace Acme.Biz.Tests
 
             // Assert
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void PlaceOrderTest()
+        {
+            // Arrange
+            var vender = new Vendor();
+            var product = new Product(1, "Hammer", "");
+            var expected = new OperationResult(true, "Order from ACME, Inc\nProduct: Tools-1\nQuantity: 1");
+
+            // Act
+            var actual = vender.PlaceOrder(product, 1);
+
+            // Assert
+            Assert.AreEqual(expected.Success, actual.Success);
+            Assert.AreEqual(expected.Message, actual.Message);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PlaceOrderTest_NullProduct_Exception()
+        {
+            // Arrange
+            var vender = new Vendor();
+
+            // Act
+            var actual = vender.PlaceOrder(null, 12);
+
+            // Assert
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void PlaceOrderTest_ImproperQuantity_Exception()
+        {
+            // Arrange
+            var vender = new Vendor();
+
+            // Act
+            var actual = vender.PlaceOrder(new Product(1, "Hammer", ""), 0);
+
+            // Assert
         }
     }
 }
