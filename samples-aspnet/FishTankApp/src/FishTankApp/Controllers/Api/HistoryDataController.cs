3adaa9b835280a2a011222da86a5e58c977b64da
James <jimxshaw@gmail.com>
2016-01-26 10:46:43 -0600

    implemented HistoryController.cs and HistoryDataController.cs. added jquery to utilize morris

---
+++ samples-aspnet/FishTankApp/src/FishTankApp/Controllers/Api/HistoryDataController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FishTankApp.Models;
+using FishTankApp.Services;
+using Microsoft.AspNet.Mvc;
+
+namespace FishTankApp.Controllers.Api
+{
+    // In asp.net 5, web api and mvc are unified. Where in previous versions, a
+    // controller used in a web api would inherit from ApiController, it now 
+    // inherits simply from Controller instead. 
+    public class HistoryDataController : Controller
+    {
+        private readonly ISensorDataService _sensorDataService;
+
+        public HistoryDataController(ISensorDataService sensorDataService)
+        {
+            _sensorDataService = sensorDataService;
+        }
+
+        public IEnumerable<IntHistoryModel> GetWaterTemperatureHistory()
+        {
+            return _sensorDataService.GetWaterTemperatureFahrenheitHistory();
+        }
+
+        public IEnumerable<IntHistoryModel> GetFishMotionPercentageHistory()
+        {
+            return _sensorDataService.GetFishMotionPercentageHistory();
+        }
+
+        public IEnumerable<IntHistoryModel> GetWaterOpacityPercentageHistory()
+        {
+            return _sensorDataService.GetWaterOpacityPercentageHistory();
+        }
+        public IEnumerable<IntHistoryModel> GetLightIntensityLumensHistory()
+        {
+            return _sensorDataService.GetLightIntensityLumensHistory();
+        }
+    }
+}
---
+++ samples-aspnet/FishTankApp/src/FishTankApp/Controllers/HistoryController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FishTankApp.ViewModels;
+using Microsoft.AspNet.Mvc;
+
+namespace FishTankApp.Controllers
+{
+    public class HistoryController : Controller
+    {
+        public IActionResult GetWaterTemperatureChart()
+        {
+            return View("Chart", new ChartViewModel
+            {
+                Title = "Water Temperature",
+                DataUrl = Url.Action("GetWaterTemperatureHistory", "HistoryData")
+            });
+        }
+
+        public IActionResult GetFishMotionPercentageChart()
+        {
+            return View("Chart", new ChartViewModel
+            {
+                Title = "Fish Motion",
+                DataUrl = Url.Action("GetFishMotionPercentageHistory", "HistoryData")
+            });
+        }
+
+        public IActionResult GetWaterOpacityPercentageChart()
+        {
+            return View("Chart", new ChartViewModel
+            {
+                Title = "Water Opacity",
+                DataUrl = Url.Action("GetWaterOpacityPercentageHistory", "HistoryData")
+            });
+        }
+
+        public IActionResult GetLightIntensityLumensChart()
+        {
+            return View("Chart", new ChartViewModel
+            {
+                Title = "Light Intensity",
+                DataUrl = Url.Action("GetLightIntensityLumensHistory", "HistoryData")
+            });
+        }
+    }
+}
