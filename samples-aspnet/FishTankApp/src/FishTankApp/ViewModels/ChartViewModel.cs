82e0f030ebf934b246147ddd5d9f562509ae5460
James <jimxshaw@gmail.com>
2016-01-26 10:22:40 -0600

    added ChartViewModel.cs and Chart.cshtml view

---
+++ samples-aspnet/FishTankApp/src/FishTankApp/ViewModels/ChartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
+
+namespace FishTankApp.ViewModels
+{
+    public class ChartViewModel
+    {
+        public string Title { get; set; }
+        public string DataUrl { get; set; }
+    }
+}
