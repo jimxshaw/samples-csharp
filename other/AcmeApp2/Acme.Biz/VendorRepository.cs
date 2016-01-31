f5fdb862bd5e2072d198823b756f4b4a7fdea050
James <jimxshaw@gmail.com>
2016-01-31 11:24:58 -0600

    created RetrieveValue<T> generic method. added unit tests to test that method

--- other/AcmeApp2/Acme.Biz/VendorRepository.cs
+++ other/AcmeApp2/Acme.Biz/VendorRepository.cs
@@ -29,6 +29,19 @@ namespace Acme.Biz
             return vendor;
         }
 
+        // The below method returns a generic type T. That type <T> is defined on 
+        // the method itself as opposed to directly on the class is because no other 
+        // class members are generic. Defining <T> only on the method that uses T 
+        // simplifies things.  
+        public T RetrieveValue<T>(string sql, T defaultValue)
+        {
+            // Call the database to retrieve the value
+            // If no value is returned, return the default value
+            T value = defaultValue;
+
+            return value;
+        }
+
         /// <summary>
         /// Save data for one vendor.
         /// </summary>
