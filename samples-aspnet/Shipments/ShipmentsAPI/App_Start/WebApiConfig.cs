f73721a101dc8cd9238f7e3ea8029e0b1b65e860
James <jimxshaw@gmail.com>
2016-02-12 12:53:58 -0600

    added Startup.cs and WebApiConfig.cs for routing

---
+++ samples-aspnet/Shipments/ShipmentsAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace ShipmentsAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            // This is our default route.
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+        }
+    }
+}
---
+++ samples-aspnet/Shipments/ShipmentsAPI/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+using ShipmentsAPI;
+
+// This is the Owin startup class that will be fired when the server starts.
+// The assembly attribute states that this'll be the class to be fired on startup.
+[assembly: OwinStartup(typeof(Startup))]
+namespace ShipmentsAPI
+{
+    public class Startup
+    {
+        // The IAppBuilder parameter is an interface, which will be used to compose the 
+        // application for our Owin server. 
+        public void Configuration(IAppBuilder app)
+        {
+            // The HttpConfiguration object will be used to configure our api routes. 
+            var config = new HttpConfiguration();
+
+            WebApiConfig.Register(config);
+
+            // AllowAll is not recommended for production versions but since this is 
+            // for development we'll allow all origins to call our api.  
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+    }
+}
