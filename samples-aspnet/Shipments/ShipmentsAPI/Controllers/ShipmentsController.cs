dcffbd630c45dd6a6400fde93c07846def9b3adb
James <jimxshaw@gmail.com>
2016-02-12 13:26:06 -0600

    added ShipmentsController. tested Get() method

---
+++ samples-aspnet/Shipments/ShipmentsAPI/Controllers/ShipmentsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ShipmentsAPI.Controllers
+{
+    // Unauthenticated users cannot call this controller.
+    [Authorize]
+    public class ShipmentsController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
