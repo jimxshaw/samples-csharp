0215174f6a54fda99159a971fbda9dd3e1ab25eb
James <jimxshaw@gmail.com>
2016-02-24 21:27:58 -0600

    created ACM solution. added ACM.BL & ACM.BL.Tests

---
+++ other/ACM/ACM.BL.Tests/CustomerTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BL.Tests
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            // Arrange
+            var customer = new Customer();
+            customer.FirstName = "Thomas";
+            customer.LastName = "Jefferson";
+
+            var expected = "Jefferson, Thomas";
+
+            // Act
+            var actual = customer.FullName;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FirstNameTestValid()
+        {
+            // Arrange
+            var customer = new Customer();
+            customer.FirstName = "Thomas";
+
+            var expected = "Thomas";
+
+            // Act
+            var actual = customer.FullName;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void LastNameTestValid()
+        {
+            // Arrange
+            var customer = new Customer();
+            customer.LastName = "Jefferson";
+
+            var expected = "Jefferson";
+
+            // Act
+            var actual = customer.FullName;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void StaticTest()
+        {
+            // Arrange
+            var c1 = new Customer();
+            c1.FirstName = "James";
+            Customer.InstanceCount += 1;
+
+            var c2 = new Customer();
+            c2.FirstName = "John";
+            Customer.InstanceCount += 1;
+
+            var c3 = new Customer();
+            c3.FirstName = "George";
+            Customer.InstanceCount += 1;
+
+            var expected = 3;
+
+            // Act
+            var actual = Customer.InstanceCount;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
