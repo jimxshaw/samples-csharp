c5b7c24408411fdaf1c051e830a310c28e5d4285
James <jimxshaw@gmail.com>
2016-03-14 15:29:43 -0500

    added asp.net core sample boilerplate.

---
+++ samples-aspnet/OdeToFood/src/OdeToFood/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OdeToFood
+{
+    public class Startup
+    {
+        public Startup()
+        {
+            // The purpose of a configuration builder is so that we can use it to define configurable sources 
+            // for our application. We can chain on different sources. 
+            var builder = new ConfigurationBuilder()
+                            .AddJsonFile("appsettings.json");
+
+            // Use the configuration property and build up the data structure that will represent the 
+            // configuration that we'll need. 
+            Configuration = builder.Build();
+        }
+
+
+        public IConfiguration Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseIISPlatformHandler();
+
+            app.Run(async (context) =>
+            {
+                // The greeting key comes from the appsettings.json file we added to configurable builder.
+                var greeting = Configuration["greeting"];
+                await context.Response.WriteAsync(greeting);
+            });
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
