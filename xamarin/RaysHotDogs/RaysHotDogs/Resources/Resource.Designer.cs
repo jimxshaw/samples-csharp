e562ca6aa39db6cf61e38767e05b1f31ead6236b
Jim Shaw <jimxshaw@gmail.com>
2016-07-11 12:10:11 -0500

    added hot dog detail activity and layout

--- xamarin/RaysHotDogs/RaysHotDogs/Resources/Resource.Designer.cs
+++ xamarin/RaysHotDogs/RaysHotDogs/Resources/Resource.Designer.cs
@@ -60,8 +60,23 @@ namespace RaysHotDogs
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050005
+			public const int MyButton = 2131034117;
+			
 			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
+			public const int image_view_hot_dog = 2131034112;
+			
+			// aapt resource value: 0x7f050003
+			public const int text_view_description = 2131034115;
+			
+			// aapt resource value: 0x7f050001
+			public const int text_view_hot_dog_name = 2131034113;
+			
+			// aapt resource value: 0x7f050004
+			public const int text_view_price = 2131034116;
+			
+			// aapt resource value: 0x7f050002
+			public const int text_view_short_description = 2131034114;
 			
 			static Id()
 			{
@@ -77,7 +92,10 @@ namespace RaysHotDogs
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int HotDogDetailView = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
 			
 			static Layout()
 			{
