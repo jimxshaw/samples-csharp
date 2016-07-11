0c6b43cc227acedcbf600d43b33a04e7022a7a22
Jim Shaw <jimxshaw@gmail.com>
2016-07-11 13:45:45 -0500

    captured widgets in hot dog detail activity

--- xamarin/RaysHotDogs/RaysHotDogs/HotDogDetailActivity.cs
+++ xamarin/RaysHotDogs/RaysHotDogs/HotDogDetailActivity.cs
@@ -9,17 +9,58 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using RaysHotDogs.Core.Model;
+using RaysHotDogs.Core.Service;
 
 namespace RaysHotDogs
 {
     [Activity(Label = "Hot dog detail", MainLauncher = true)]
     public class HotDogDetailActivity : Activity
     {
+
+        private ImageView mImageViewHotDog;
+        private TextView mTextViewHotDogName;
+        private TextView mTextViewShortDescription;
+        private TextView mTextViewDescription;
+        private TextView mTextViewPrice;
+        private EditText mEditTextAmount;
+        private Button mButtonCancel;
+        private Button mButtonOrder;
+
+        private HotDog mSelectedHotDog;
+        private HotDogDataService mDataService;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.HotDogDetailView);
 
-            // Create your application here
+            mDataService = new HotDogDataService();
+            mSelectedHotDog = mDataService.GetHotDogById(1);
+
+            FindViews();
+            BindData();
+
+        }
+
+        private void FindViews()
+        {
+            mImageViewHotDog = FindViewById<ImageView>(Resource.Id.image_view_hot_dog);
+            mTextViewHotDogName = FindViewById<TextView>(Resource.Id.text_view_hot_dog_name);
+            mTextViewShortDescription = FindViewById<TextView>(Resource.Id.text_view_short_description);
+            mTextViewDescription = FindViewById<TextView>(Resource.Id.text_view_description);
+            mTextViewPrice = FindViewById<TextView>(Resource.Id.text_view_price);
+            mEditTextAmount = FindViewById<EditText>(Resource.Id.edit_text_amount);
+            mButtonCancel = FindViewById<Button>(Resource.Id.button_cancel);
+            mButtonOrder = FindViewById<Button>(Resource.Id.button_order);
+        }
+
+        private void BindData()
+        {
+            mTextViewHotDogName.Text = mSelectedHotDog.Name;
+            mTextViewShortDescription.Text = mSelectedHotDog.ShortDescription;
+            mTextViewDescription.Text = mSelectedHotDog.Description;
+            mTextViewPrice.Text = "Price: " + mSelectedHotDog.Price;
         }
     }
 }
