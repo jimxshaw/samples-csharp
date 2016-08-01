c63051d86881490d9269bf970a12973fea7f1238
Jim Shaw <jimxshaw@gmail.com>
2016-08-01 13:31:43 -0600

    Initial commit.

---
+++ MeetHub/MeetHub/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MeetHub.MvcApplication" Language="C#" %>
---
+++ MeetHub/MeetHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MeetHub.Startup))]
+namespace MeetHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
