7cdb4521b94fb674ce159c1c6c32d2229d2729af
Jim Shaw <jimxshaw@gmail.com>
2016-08-02 14:46:25 -0500

    Added HttpPost Create action in MeetupsController.

--- MeetHub/MeetHub/Controllers/MeetupsController.cs
+++ MeetHub/MeetHub/Controllers/MeetupsController.cs
@@ -1,5 +1,7 @@
 ﻿using MeetHub.Models;
 using MeetHub.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -23,6 +25,28 @@ namespace MeetHub.Controllers
             };
 
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(MeetupFormViewModel viewModel)
+        {
+            var groupId = User.Identity.GetUserId();
+            var group = _context.Users.Single(u => u.Id == groupId);
+            var category = _context.Categories.Single(c => c.Id == viewModel.Category);
+            var meetup = new Meetup
+            {
+                Group = group,
+                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                Category = category,
+                Venue = viewModel.Venue,
+                Title = viewModel.Title,
+                Description = viewModel.Description
+            };
+
+            _context.Meetups.Add(meetup);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
