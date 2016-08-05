2a513dd3f6be9ed58d4e18674a3bce9fa9d915cc
Jim Shaw <jimxshaw@gmail.com>
2016-08-05 14:18:49 -0500

    Added Attendance class. Added that to our ApplicationDbContext class. Added a migration and updated our database.

--- MeetHub/MeetHub/Models/ApplicationDbContext.cs
+++ MeetHub/MeetHub/Models/ApplicationDbContext.cs
@@ -1,5 +1,5 @@
-using System.Data.Entity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 namespace MeetHub.Models
 {
@@ -7,6 +7,9 @@ namespace MeetHub.Models
     {
         public DbSet<Meetup> Meetups { get; set; }
         public DbSet<Category> Categories { get; set; }
+        // An attendance is contains a meetup and an application user. It's essentially 
+        // the cross table between those two objects. 
+        public DbSet<Attendance> Attendances { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
@@ -16,6 +19,18 @@ namespace MeetHub.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // An attendance has to have a meetup. We'll never delete any meetups though, only cancel them.
+            // So we'll have to turn off cascade on delete.
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Meetup).WithMany()
+                .WillCascadeOnDelete(false);
+
+            // Be sure to call base last.
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
---
+++ MeetHub/MeetHub/Models/Attendance.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MeetHub.Models
+{
+    public class Attendance
+    {
+        public Meetup Meetup { get; set; }
+        public ApplicationUser Attendee { get; set; }
+
+        // Foreign key constraints to this many-to-many relationship. 
+        // The composite key of a MeetupId plus an AttendeeId represents a unique 
+        // Attendance object. A composite key also requires the column order to be specified.
+        [Key]
+        [Column(Order = 1)]
+        public int MeetupId { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        public string AttendeeId { get; set; }
+    }
+}
