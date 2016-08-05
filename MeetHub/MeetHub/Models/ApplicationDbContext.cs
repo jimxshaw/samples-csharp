ef2e8cf2c97b6b4059c15fd4deeb5a6bdafe1ffa
Jim Shaw <jimxshaw@gmail.com>
2016-08-05 18:21:18 -0500

    Created Following class. Added Follower and Followee to ApplicationDbContext and ApplicationUser. Added migration and updated database.

--- MeetHub/MeetHub/Models/ApplicationDbContext.cs
+++ MeetHub/MeetHub/Models/ApplicationDbContext.cs
@@ -10,6 +10,8 @@ namespace MeetHub.Models
         // An attendance is contains a meetup and an application user. It's essentially 
         // the cross table between those two objects. 
         public DbSet<Attendance> Attendances { get; set; }
+        public DbSet<Following> Followings { get; set; }
+
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
@@ -27,6 +29,18 @@ namespace MeetHub.Models
             // So we'll have to turn off cascade on delete.
             modelBuilder.Entity<Attendance>()
                 .HasRequired(a => a.Meetup).WithMany()
+                .WillCascadeOnDelete(false);
+
+            // An application user has many followers. Each follower has a required followee.
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followers)
+                .WithRequired(f => f.Followee)
+                .WillCascadeOnDelete(false);
+
+            // An application user has many followees. Each followee has a required follower.
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followees)
+                .WithRequired(f => f.Follower)
                 .WillCascadeOnDelete(false);
 
             // Be sure to call base last.
--- MeetHub/MeetHub/Models/ApplicationUser.cs
+++ MeetHub/MeetHub/Models/ApplicationUser.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -13,6 +15,17 @@ namespace MeetHub.Models
         [Required]
         [StringLength(100)]
         public string Name { get; set; }
+
+        // Whenever we add a property and that property is a collection, we should already initialize it in the constructor. 
+        // That's a key responsibility of the class.
+        public ICollection<Following> Followers { get; set; }
+        public ICollection<Following> Followees { get; set; }
+
+        public ApplicationUser()
+        {
+            Followers = new Collection<Following>();
+            Followees = new Collection<Following>();
+        }
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
