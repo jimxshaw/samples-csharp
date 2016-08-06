fe79cc2b384e301227baf969b5ea69213f1292df
Jim Shaw <jimxshaw@gmail.com>
2016-08-05 19:10:59 -0500

    Modified CSS for Following button. Created web api FollowingsController and FollowingDataTransferObject.

---
+++ MeetHub/MeetHub/Controllers/FollowingsController.cs
@@ -0,0 +1,41 @@
+﻿using MeetHub.DTOs;
+using MeetHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace MeetHub.Controllers
+{
+    [Authorize]
+    public class FollowingsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public FollowingsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDataTransferObject dto)
+        {
+            var userIdString = User.Identity.GetUserId();
+
+            if (_context.Followings.Any(f => f.FolloweeId == userIdString && f.FolloweeId == dto.FolloweeId))
+            {
+                return BadRequest("Following already exists.");
+            }
+
+            var following = new Following()
+            {
+                FollowerId = userIdString,
+                FolloweeId = dto.FolloweeId
+            };
+
+            _context.Followings.Add(following);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ MeetHub/MeetHub/DTOs/FollowingDataTransferObject.cs
@@ -0,0 +1,8 @@
+namespace MeetHub.DTOs
+{
+    public class FollowingDataTransferObject
+    {
+        public string FolloweeId { get; set; }
+
+    }
+}
