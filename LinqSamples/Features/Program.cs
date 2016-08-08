eae9704bffeaa3f5ca4bc2047385da354c02ffd6
Jim Shaw <jimxshaw@gmail.com>
2016-08-08 14:40:01 -0500

    Added Features project to LinqSamples solution.

---
+++ LinqSamples/Features/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Employee> sales = new Employee[]
+            {
+                new Employee()
+                {
+                    Id = 1,
+                    Name = "Lara"
+                },
+                new Employee()
+                {
+                    Id = 2,
+                    Name = "George"
+                }
+            };
+
+            IEnumerable<Employee> developers = new List<Employee>()
+            {
+                new Employee()
+                {
+                    Id = 1,
+                    Name = "James"
+                },
+                new Employee()
+                {
+                    Id = 2,
+                    Name = "Marcus"
+                },
+                new Employee()
+                {
+                    Id = 3,
+                    Name = "Beth"
+                }
+            };
+
+            IEnumerator<Employee> enumerator = developers.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Name);
+            }
+        }
+    }
+}
