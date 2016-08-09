6cb0ef022ea92b1727899a69b806eea7d63cb180
Jim Shaw <jimxshaw@gmail.com>
2016-08-08 21:19:43 -0500

    Tested LINQ with Func<> and Action<>.

---
+++ LinqSamples/Features/MyLinq.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Features
+{
+    public static class MyLinq
+    {
+        // The this keyword in front of the first parameter signals that this method is an extension method.
+        // We could call Count either using the usual static way with MyLinq.Count(sequence) or, in any place 
+        // we have IEnumerble<T>, simply invoke .Count(sequence). We can invoke with myArray.Count() or 
+        // myList.Count() or with any other IEnumerable collection.  
+        public static int Count<T>(this IEnumerable<T> sequence)
+        {
+            int counter = 0;
+            foreach (var item in sequence)
+            {
+                counter += 1;
+            }
+
+            return counter;
+        }
+    }
+}
--- LinqSamples/Features/Program.cs
+++ LinqSamples/Features/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Features
 {
@@ -37,14 +38,41 @@ namespace Features
                 {
                     Id = 3,
                     Name = "Beth"
+                },
+                new Employee()
+                {
+                    Id = 4,
+                    Name = "Joseph"
+                },
+                new Employee()
+                {
+                    Id = 5,
+                    Name = "Alex"
+                },
+                new Employee()
+                {
+                    Id = 6,
+                    Name = "Jose"
                 }
             };
 
-            IEnumerator<Employee> enumerator = developers.GetEnumerator();
-            while (enumerator.MoveNext())
+            // Func types typically return types.
+            Func<int, int> cube = x => x * x * x;
+            Func<int, int, int> subtract = (x, y) => x - y;
+
+            // Action types are always void so we only provide the types going in.
+            Action<int> writeOut = x => Console.WriteLine(x);
+
+            Console.WriteLine(cube(3));
+            Console.WriteLine(subtract(10, 8));
+
+            writeOut(5);
+
+            foreach (var employee in developers.Where(e => e.Name.Length == 4).OrderByDescending(e => e.Name))
             {
-                Console.WriteLine(enumerator.Current.Name);
+                Console.WriteLine($"{employee.Id} - {employee.Name}");
             }
         }
+
     }
 }
