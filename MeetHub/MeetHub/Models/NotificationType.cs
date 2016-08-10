d112520708da0e0a8343904f71ea0c7b7e80fc14
Jim Shaw <jimxshaw@gmail.com>
2016-08-10 16:43:09 -0500

    Created Notification, NotificationType, UserNotification model classes.

---
+++ MeetHub/MeetHub/Models/NotificationType.cs
@@ -0,0 +1,9 @@
+﻿namespace MeetHub.Models
+{
+    public enum NotificationType
+    {
+        MeetupCancelled = 1,
+        MeetupUpdated = 2,
+        MeetupCreated = 3
+    }
+}
---
+++ MeetHub/MeetHub/Models/UserNotification.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MeetHub.Models
+{
+    // This is the cross table between the User and the Notification.
+    public class UserNotification
+    {
+        // We must explicitly state the composite primary keys. As usual,
+        // when we use the Key attribute we must also explicitly order of the 
+        // columns. 
+        [Key]
+        [Column(Order = 1)]
+        public string UserId { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        public int NotificationId { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        public Notification Notification { get; set; }
+
+        public bool IsRead { get; set; }
+    }
+}
