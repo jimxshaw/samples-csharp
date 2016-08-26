12623130ba15222e72d94e74d105d6a2240331a7
Jim Shaw <jimxshaw@gmail.com>
2016-08-25 20:53:06 -0500

    Re installed AutoMapper 5. Created MappingProfile class. Added Mapper.Initialize to Global.asax.cs.

---
+++ MeetHub/MeetHub/App_Start/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MeetHub.Controllers.Api;
+using MeetHub.Models;
+
+namespace MeetHub
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<ApplicationUser, UserDataTransferObject>();
+            CreateMap<Category, CategoryDataTransferObject>();
+            CreateMap<Meetup, MeetupDataTransferObject>();
+            CreateMap<Notification, NotificationDataTransferObject>();
+        }
+    }
+}
--- MeetHub/MeetHub/Global.asax.cs
+++ MeetHub/MeetHub/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Http;
+﻿using AutoMapper;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -9,6 +10,8 @@ namespace MeetHub
     {
         protected void Application_Start()
         {
+            Mapper.Initialize(cfg => cfg.AddProfile<MappingProfile>());
+
             // After creating our first web api 2 controller, paste the following line in the 
             // readme file here.
             GlobalConfiguration.Configure(WebApiConfig.Register);
