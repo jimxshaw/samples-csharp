ff86409de51a4e3c49e233f572a7c9b9e65bfa08
Jim Shaw <jimxshaw@gmail.com>
2016-09-02 12:33:00 -0500

    Created LoggingServiceTest class and tested the WriteToFile method.

---
+++ other/ACM/Acme.CommonTest/LoggingServiceTest.cs
@@ -0,0 +1,43 @@
+﻿using ACM.BL;
+using Acme.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Acme.CommonTest
+{
+    [TestClass]
+    public class LoggingServiceTest
+    {
+        [TestMethod]
+        public void WriteToFileTest()
+        {
+            // Arrange
+            var changedItems = new List<ILoggable>();
+
+            var customer = new Customer(1)
+            {
+                FirstName = "John",
+                LastName = "Lennon",
+                EmailAddress = "johnlennon@beatles.com",
+                AddressList = null
+            };
+
+            var product = new Product()
+            {
+                ProductName = "Guitar",
+                CurrentPrice = 399.99M,
+                ProductDescription = "Gibson Les Paul"
+            };
+
+            changedItems.Add(customer as ILoggable);
+            changedItems.Add(product as ILoggable);
+
+
+            // Act
+            LoggingService.WriteToFile(changedItems);
+
+            // Assert
+            // Nothing to assert in this case.
+        }
+    }
+}
