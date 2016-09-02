f76f003d771add801097ee848ec0949bb0f385c4
Jim Shaw <jimxshaw@gmail.com>
2016-09-02 11:59:44 -0500

    Created LoggingService class in Acme.Common project.

---
+++ other/ACM/Acme.CommonTest/LoggingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Acme.CommonTest
+{
+    public static class LoggingService
+    {
+        public static void WriteToFile(List<object> changedItems)
+        {
+            foreach (var item in changedItems)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
