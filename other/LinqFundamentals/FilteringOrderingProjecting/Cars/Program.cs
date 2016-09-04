017d24ce96c7601ad4824800793dca2885a38f29
Jim Shaw <jimxshaw@gmail.com>
2016-09-04 17:37:51 -0500

    Created custom CarExtensions class.

--- other/LinqFundamentals/FilteringOrderingProjecting/Cars/Program.cs
+++ other/LinqFundamentals/FilteringOrderingProjecting/Cars/Program.cs
@@ -11,44 +11,74 @@ namespace Cars
         {
             var cars = ProcessFile("fuel.csv");
 
-            // Sort all cars by combined fuel efficiency descending and then sort it by 
-            // car name in ascending order.
+            var query = cars.Where(c => c.Manufacturer == "Audi" && c.Year == 2016)
+                            .OrderByDescending(c => c.Combined)
+                            .ThenBy(c => c.Name)
+                            .Select(c => c);
 
-            //            var query = cars.OrderByDescending(car => car.Combined)
-            //                            .ThenBy(c => c.Name)
-            //                            .ToList();
+            //            var query = from car in cars
+            //                        where car.Manufacturer == "BMW"
+            //                        orderby car.Combined descending, car.Name ascending
+            //                        select car;
 
-            var query = from car in cars
-                        orderby car.Combined descending, car.Name ascending
-                        select car;
+            var result = cars.Any(c => c.Manufacturer == "Cherry");
 
-            foreach (var car in query.ToList().Take(10))
-            {
-                Console.WriteLine($"{car.Name} : {car.Combined}");
-            }
+            Console.WriteLine(result);
+
+            //            foreach (var car in query)
+            //            {
+            //                Console.WriteLine($"{car.Name} : {car.Combined}");
+            //            }
         }
 
         private static List<Car> ProcessFile(string path)
         {
-            // Given the path to our data source file, read all lines from it if 
-            // the line's length is at least 1. Also, skip the first line because 
-            // that's the file column header line. Then map each line of type string 
-            // into an object of type Car by using the Car's own ParseFromCsv method that we wrote 
-            // ourselves. We could have passed into Select a complex lambda expression 
-            // but using a method is easier.
 
-            //            return File.ReadAllLines(path)
-            //                .Skip(1)
-            //                .Where(line => line.Length > 1)
-            //                .Select(Car.ParseFromCsv).ToList();
+            var query = File.ReadAllLines(path)
+                .Skip(1)
+                .Where(line => line.Length > 1)
+                .ToCar();
 
             // We could use the query syntax as well.
-            var queryResult = from line in File.ReadAllLines(path).Skip(1)
-                              where line.Length > 1
-                              select Car.ParseFromCsv(line);
+            //            var queryResult = from line in File.ReadAllLines(path).Skip(1)
+            //                              where line.Length > 1
+            //                              select Car.ParseFromCsv(line);
 
-            return queryResult.ToList();
+            //            return queryResult.ToList();
+
+            return query.ToList();
         }
 
+    }
+
+    // This class will have custom linq operators for when we're creating or querying 
+    // our list of cars.
+    public static class CarExtensions
+    {
+        public static IEnumerable<Car> ToCar(this IEnumerable<string> source)
+        {
+            // We're assuming we are reading lines from a file.
+            foreach (var line in source)
+            {
+                // Take each line (column) in the csv file and split it by the comma character.
+                // Note that the comma is of type char, not string.
+                var columns = line.Split(',');
+
+                // Given an incoming source of data, execute some code to transform a string into 
+                // a Car type.
+                yield return new Car()
+                {
+                    Year = int.Parse(columns[0]),
+                    Manufacturer = columns[1],
+                    Name = columns[2],
+                    Displacement = double.Parse(columns[3]),
+                    Cylinders = int.Parse(columns[4]),
+                    City = int.Parse(columns[5]),
+                    Highway = int.Parse(columns[6]),
+                    Combined = int.Parse(columns[7])
+                };
+            }
+
+        }
     }
 }
