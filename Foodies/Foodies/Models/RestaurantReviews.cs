9cded66af10a57f9957121fa302e0ebcfa042649
Jim Shaw <jimxshaw@gmail.com>
2016-09-06 10:55:43 -0500

    Added ReviewsController and RestaurantReviews model class.

---
+++ Foodies/Foodies/Models/RestaurantReviews.cs
@@ -0,0 +1,13 @@
+﻿namespace Foodies.Models
+{
+    public class RestaurantReviews
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Country { get; set; }
+        public int Rating { get; set; }
+    }
+}
