3ad9bcf42be1618aa9fd06c25e21975274aca0cd
Jim Shaw <jimxshaw@gmail.com>
2016-09-09 16:51:30 -0500

    Added Demo model class.

---
+++ other/Globalization/Globalization/Models/Demo.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Globalization.Models
+{
+    public class Demo
+    {
+        [DisplayName("Name")]
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [DisplayName("Age")]
+        [Required(ErrorMessage = "Age is required")]
+        public int Age { get; set; }
+    }
+}
