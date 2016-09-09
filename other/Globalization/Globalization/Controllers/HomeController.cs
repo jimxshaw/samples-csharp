16cd1cb074fbeb014a07404ccb4a559e338f207f
Jim Shaw <jimxshaw@gmail.com>
2016-09-09 16:54:47 -0500

    Added Create HttpGet and HttpPost actions in HomeController.

--- other/Globalization/Globalization/Controllers/HomeController.cs
+++ other/Globalization/Globalization/Controllers/HomeController.cs
@@ -1,16 +1,16 @@
-﻿using System;
+﻿using Globalization.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Globalization.Controllers
 {
     public class HomeController : Controller
     {
+        private static List<Demo> list = new List<Demo>();
+
         public ActionResult Index()
         {
-            return View();
+            return View(list);
         }
 
         public ActionResult About()
@@ -23,6 +23,25 @@ namespace Globalization.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Demo model)
+        {
+            if (ModelState.IsValid)
+            {
+                list.Add(model);
+
+                return RedirectToAction("Index");
+            }
 
             return View();
         }
