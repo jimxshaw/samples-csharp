16728d43ee8ee2e4a46f2229a1fcec7bf3e08276
Jim Shaw <jimxshaw@gmail.com>
2016-09-13 11:45:59 -0500

    Added Logger and UI classes that implement IObserver interface.

---
+++ other/DesignPatterns/ManagingResponsibilities/ObserverDemo/ObserverDemo/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObserverDemo
+{
+    public class Logger : IObserver
+    {
+        public void Update()
+        {
+            Console.WriteLine("Updated has been written and logged.");
+        }
+    }
+}
---
+++ other/DesignPatterns/ManagingResponsibilities/ObserverDemo/ObserverDemo/UserInterface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObserverDemo
+{
+    public class UserInterface : IObserver
+    {
+        public void Update()
+        {
+            Console.WriteLine("Hello user, something has been updated!");
+        }
+    }
+}
