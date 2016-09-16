7991df03e9835d71bb90fcca221b2791c5b51f72
Jim Shaw <jimxshaw@gmail.com>
2016-09-16 12:56:22 -0500

    Added transitions to toggle sidebar.

--- samples-aspnet/TheGlobe/src/TheGlobe/wwwroot/js/site.js
+++ samples-aspnet/TheGlobe/src/TheGlobe/wwwroot/js/site.js
@@ -4,6 +4,12 @@
 
     $("#sidebarToggle").on("click", function() {
         $sidebarAndWrapper.toggleClass("hide-sidebar");
+
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
     });
     
 })();
