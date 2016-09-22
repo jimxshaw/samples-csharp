43cc6e7ada651820ae4d8ca76496ec495ab94490
Jim Shaw <jimxshaw@gmail.com>
2016-09-22 11:54:11 -0500

    Used dotnet ef to add initial database migration.

---
+++ samples-aspnet/TheGlobe/src/TheGlobe/Migrations/GlobeContextModelSnapshot.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using TheGlobe.Models;
+
+namespace TheGlobe.Migrations
+{
+    [DbContext(typeof(GlobeContext))]
+    partial class GlobeContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("TheGlobe.Models.Stop", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("Arrival");
+
+                    b.Property<double>("Latitude");
+
+                    b.Property<double>("Longitude");
+
+                    b.Property<string>("Name");
+
+                    b.Property<int>("Order");
+
+                    b.Property<int?>("TripId");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("TripId");
+
+                    b.ToTable("Stops");
+                });
+
+            modelBuilder.Entity("TheGlobe.Models.Trip", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("DateCreated");
+
+                    b.Property<string>("Name");
+
+                    b.Property<string>("UserName");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Trips");
+                });
+
+            modelBuilder.Entity("TheGlobe.Models.Stop", b =>
+                {
+                    b.HasOne("TheGlobe.Models.Trip")
+                        .WithMany("Stops")
+                        .HasForeignKey("TripId");
+                });
+        }
+    }
+}
