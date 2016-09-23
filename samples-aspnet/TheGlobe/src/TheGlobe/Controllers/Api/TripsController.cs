c63b80ea2ad8ede5b85e4d01deaa3dc520bc75ca
Jim Shaw <jimxshaw@gmail.com>
2016-09-23 11:07:05 -0500

    Create TripsController api controller.

---
+++ samples-aspnet/TheGlobe/src/TheGlobe/Controllers/Api/TripsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheGlobe.Models;
+
+namespace TheGlobe.Controllers.Api
+{
+    public class TripsController : Controller
+    {
+        public JsonResult Get()
+        {
+            return Json(new Trip()
+            {
+                Name = "My Trip"
+            });
+        }
+    }
+}
