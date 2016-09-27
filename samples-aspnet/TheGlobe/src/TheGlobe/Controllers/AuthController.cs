9b29424bed052f934db56bc097f3689436593989
Jim Shaw <jimxshaw@gmail.com>
2016-09-26 19:13:25 -0500

    Implemented HttpPost Login action in AuthController.

--- samples-aspnet/TheGlobe/src/TheGlobe/Controllers/AuthController.cs
+++ samples-aspnet/TheGlobe/src/TheGlobe/Controllers/AuthController.cs
@@ -3,17 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using TheGlobe.Models;
+using TheGlobe.ViewModels;
 
 namespace TheGlobe.Controllers
 {
     public class AuthController : Controller
     {
+        private SignInManager<GlobeUser> _signInManager;
+
+        public AuthController(SignInManager<GlobeUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         public IActionResult Login()
         {
             if (User.Identity.IsAuthenticated)
             {
                 return RedirectToAction("Trips", "App");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel viewModel, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var signInResult = await _signInManager.PasswordSignInAsync(
+                    viewModel.Username, 
+                    viewModel.Password, 
+                    true, // Persist the sign in after login?
+                    false); // Lock out the user if login failed?
+
+                if (signInResult.Succeeded)
+                {
+                    if (string.IsNullOrWhiteSpace(returnUrl))
+                    {
+                        return RedirectToAction("Trips", "App");
+                    }
+
+                    return Redirect(returnUrl);
+                }
+
+                ModelState.AddModelError("", "Username or password incorrect");
             }
 
             return View();
