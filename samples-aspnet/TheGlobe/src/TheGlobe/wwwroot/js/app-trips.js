336177f7876aac624d6a208051e88373de4762ed
Jim Shaw <jimxshaw@gmail.com>
2016-09-30 15:27:40 -0500

    Added custom angular directive waitCursor.

--- samples-aspnet/TheGlobe/src/TheGlobe/wwwroot/js/app-trips.js
+++ samples-aspnet/TheGlobe/src/TheGlobe/wwwroot/js/app-trips.js
@@ -4,5 +4,5 @@
 
     // Modules are defined with a name and an array that could be 
     // filled with dependencies.
-    angular.module("app-trips", []);
+    angular.module("app-trips", ["simpleControls"]);
 })();
---
+++ samples-aspnet/TheGlobe/src/TheGlobe/wwwroot/js/simpleControls.js
@@ -0,0 +1,16 @@
+﻿// simpleControls.js
+(function() {
+    "use strict";
+
+    angular.module("simpleControls", [])
+        .directive("waitCursor", waitCursor);
+
+    function waitCursor() {
+        // Directives return an object with well know properties in it
+        // that are used to define what should be used in the directive.
+        return {
+            templateUrl: "/views/waitCursor.html"
+        };
+    }
+
+})();
