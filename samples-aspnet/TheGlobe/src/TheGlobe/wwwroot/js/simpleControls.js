543669b4545a1c44534ceb131848767e23836105
Jim Shaw <jimxshaw@gmail.com>
2016-09-30 15:36:12 -0500

    Added custom attributes to custom angular directive.

--- samples-aspnet/TheGlobe/src/TheGlobe/wwwroot/js/simpleControls.js
+++ samples-aspnet/TheGlobe/src/TheGlobe/wwwroot/js/simpleControls.js
@@ -9,6 +9,10 @@
         // Directives return an object with well know properties in it
         // that are used to define what should be used in the directive.
         return {
+            scope: {
+                show: "=displayWhen"
+            }, // Scope is the object of which we're binding out waitCursor.
+            restrict: "E", // Restrict waitCursor to be only used with the element tag style e.g. <wait-cursor></wait-cursor>.
             templateUrl: "/views/waitCursor.html"
         };
     }
