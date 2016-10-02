cb0cb7c91aeb17651d73d74735106596a5b53403
Jim Shaw <jimxshaw@gmail.com>
2016-10-02 11:10:58 -0500

    Called backend trips api to display the stops of a particular trip in tripEditorView.html.

--- samples-aspnet/TheGlobe/src/TheGlobe/wwwroot/js/tripEditorController.js
+++ samples-aspnet/TheGlobe/src/TheGlobe/wwwroot/js/tripEditorController.js
@@ -6,16 +6,28 @@
     angular.module("app-trips")
         .controller("tripEditorController", tripEditorController);
 
-    function tripEditorController($routeParams) {
+    function tripEditorController($routeParams, $http) {
         var vm = this;
 
         // Keep in mind our app-trips controller calls the route parameter
         // of each trip as :tripName. So here we have to also use 
         // tripName in order for binding to work.
         vm.tripName = $routeParams.tripName;
-        vm.stop = [];
+        vm.stops = [];
         vm.errorMessage = "";
         vm.isBusy = true;
+
+        $http.get("/api/trips/" + vm.tripName + "/stops")
+            .then(function(response) {
+                // Success
+                angular.copy(response.data, vm.stops);
+            }, function (err) {
+                // Failure
+                vm.errorMessage = "Failed to load stops: " + err;
+            })
+            .finally(function () {
+                vm.isBusy = false;
+            });
     }
 
 })();
