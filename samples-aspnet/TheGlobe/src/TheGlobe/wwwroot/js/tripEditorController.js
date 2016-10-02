542ff155b28157a8fb90bbfc31d97b2b80906da9
Jim Shaw <jimxshaw@gmail.com>
2016-10-02 13:18:05 -0500

    Implemented Add a Stop button.

--- samples-aspnet/TheGlobe/src/TheGlobe/wwwroot/js/tripEditorController.js
+++ samples-aspnet/TheGlobe/src/TheGlobe/wwwroot/js/tripEditorController.js
@@ -19,7 +19,9 @@
 
         vm.newStop = {};
 
-        $http.get("/api/trips/" + vm.tripName + "/stops")
+        var url = "/api/trips/" + vm.tripName + "/stops";
+
+        $http.get(url)
             .then(function(response) {
                 // Success: copy over data for the retrieved collection of stops
                 // and then show the map featuring those stops.
@@ -32,6 +34,26 @@
             .finally(function () {
                 vm.isBusy = false;
             });
+
+        vm.addStop = function() {
+
+            vm.isBusy = true;
+
+            $http.post(url, vm.newStop)
+                .then(function(response) {
+                    // Success
+                    vm.stops.push(response.data);
+                    _showMap(vm.stops);
+                    vm.newStop = {};
+                }, function(err) {
+                    // Failure
+                    vm.errorMessage = "Failed to add new stop: " + err;
+                })
+                .finally(function () {
+                    vm.isBusy = false;
+                });
+
+        };
     }
 
     function _showMap(stops) {
@@ -52,7 +74,7 @@
             travelMap.createMap({
                 stops: mapStops,
                 selector: "#map", // This is a CSS selector stating the id attribute where we'll place our map.
-                currentStop: 1,
+                currentStop: 0,
                 initialZoom: 3
             });
         }
