b566fddd5ec92d656921981a85340a2ab4f71fb9
Jim Shaw <jimxshaw@gmail.com>
2016-10-02 09:59:29 -0500

    Added angular route dependency to app-trips.js module.

--- samples-aspnet/TheGlobe/src/TheGlobe/wwwroot/js/app-trips.js
+++ samples-aspnet/TheGlobe/src/TheGlobe/wwwroot/js/app-trips.js
@@ -4,5 +4,20 @@
 
     // Modules are defined with a name and an array that could be 
     // filled with dependencies.
-    angular.module("app-trips", ["simpleControls"]);
+    // Angular routing allows for client-side routes.
+    angular
+        .module("app-trips", ["simpleControls", "ngRoute"])
+        .config(function ($routeProvider) {
+            // A route is provide and an object to specified on how we're
+            // to use that route.
+            $routeProvider.when("/",
+            {
+                controller: "tripsController",
+                controllerAs: "vm", // This is the controller's alias.
+                templateUrl: "/views/tripsView.html"
+            });
+
+            // This is what we do if none of the routes match.
+            $routeProvider.otherwise({ redirectTo: "/" });
+        });
 })();
