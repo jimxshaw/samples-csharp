5d0b89eb96f8eb871d030611ff2f7c554ebfda7b
Jim Shaw <jimxshaw@gmail.com>
2016-10-02 12:42:07 -0500

    Included Google Maps to display our stops on a selected trip.

--- samples-aspnet/TheGlobe/src/TheGlobe/wwwroot/js/tripEditorController.js
+++ samples-aspnet/TheGlobe/src/TheGlobe/wwwroot/js/tripEditorController.js
@@ -19,8 +19,10 @@
 
         $http.get("/api/trips/" + vm.tripName + "/stops")
             .then(function(response) {
-                // Success
+                // Success: copy over data for the retrieved collection of stops
+                // and then show the map featuring those stops.
                 angular.copy(response.data, vm.stops);
+                _showMap(vm.stops);
             }, function (err) {
                 // Failure
                 vm.errorMessage = "Failed to load stops: " + err;
@@ -28,6 +30,30 @@
             .finally(function () {
                 vm.isBusy = false;
             });
+    }
+
+    function _showMap(stops) {
+        if (stops && stops.length > 0) {
+
+            // Our stops retrieved from our backend database must be mapped into an appropriate type with
+            // correct attribute specific to what the travelMap library seeks in order to display 
+            // map properly.
+            var mapStops = _.map(stops, function (item) {
+                return {
+                    lat: item.latitude,
+                    long: item.longitude,
+                    info: item.name
+                };
+            });
+
+            // Show map
+            travelMap.createMap({
+                stops: mapStops,
+                selector: "#map", // This is a CSS selector stating the id attribute where we'll place our map.
+                currentStop: 1,
+                initialZoom: 3
+            });
+        }
     }
 
 })();
