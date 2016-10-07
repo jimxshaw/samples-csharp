d2c3f08b1ddc630195bbeea28541aff2fd575cdb
Jim Shaw <jimxshaw@gmail.com>
2016-10-07 14:31:17 -0500

    Added Mvc package, services and middleware. Tested Mvc with a mock controller.

--- FoodiesCore/src/FoodiesCore/Startup.cs
+++ FoodiesCore/src/FoodiesCore/Startup.cs
@@ -29,6 +29,7 @@ namespace FoodiesCore
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
             services.AddSingleton(Configuration);
             services.AddSingleton<IGreeter, Greeter>();
         }
@@ -48,16 +49,7 @@ namespace FoodiesCore
 
             app.UseFileServer();
 
-            app.UseWelcomePage(new WelcomePageOptions()
-            {
-                Path = "/welcome"
-            });
-
-            app.Run(async (context) =>
-            {
-                var message = greeter.GetGreeting();
-                await context.Response.WriteAsync(message);
-            });
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
