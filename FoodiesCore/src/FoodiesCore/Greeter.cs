0824c035439993313426f8f195a2bd17270b0024
Jim Shaw <jimxshaw@gmail.com>
2016-10-06 22:47:13 -0500

    Implemented Greeter class to test configuration services in startup.

---
+++ FoodiesCore/src/FoodiesCore/Greeter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace FoodiesCore
+{
+    public interface IGreeter
+    {
+        string GetGreeting();
+    }
+
+    public class Greeter : IGreeter
+    {
+        private string _greeting;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _greeting = configuration["Greeting"];
+        }
+
+        public string GetGreeting()
+        {
+            return _greeting;
+        }
+    }
+}
--- FoodiesCore/src/FoodiesCore/Startup.cs
+++ FoodiesCore/src/FoodiesCore/Startup.cs
@@ -29,10 +29,15 @@ namespace FoodiesCore
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(Configuration);
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, 
+                                IHostingEnvironment env, 
+                                ILoggerFactory loggerFactory,
+                                IGreeter greeter)
         {
             loggerFactory.AddConsole();
 
@@ -43,7 +48,7 @@ namespace FoodiesCore
 
             app.Run(async (context) =>
             {
-                var message = Configuration["Greeting"];
+                var message = greeter.GetGreeting();
                 await context.Response.WriteAsync(message);
             });
         }
