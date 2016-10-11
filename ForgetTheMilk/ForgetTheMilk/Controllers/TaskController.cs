165f1dc734cc6ef2638ca7121661dbc46fe5c058
Jim Shaw <jimxshaw@gmail.com>
2016-10-10 21:00:30 -0500

    Added Add http post action in TaskController.

--- ForgetTheMilk/ForgetTheMilk/Controllers/TaskController.cs
+++ ForgetTheMilk/ForgetTheMilk/Controllers/TaskController.cs
@@ -1,14 +1,24 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace ForgetTheMilk.Controllers
 {
     public class TaskController : Controller
     {
+        public static readonly List<string> Tasks = new List<string>();
+
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Add(string task)
+        {
+            Tasks.Add(task);
+
+            return RedirectToAction("Index");
+        }
 
     }
 }
