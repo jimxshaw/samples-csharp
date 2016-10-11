cbc4d58efe124ce4b390bedc5db2f401201daf5b
Jim Shaw <jimxshaw@gmail.com>
2016-10-10 20:46:43 -0500

    Added ForgetTheMilk sample.

---
+++ ForgetTheMilk/ForgetTheMilk/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace ForgetTheMilk.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
