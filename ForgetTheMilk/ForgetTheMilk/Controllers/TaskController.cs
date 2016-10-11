72f16a40a88051794933d31d839628d773cfb2d6
Jim Shaw <jimxshaw@gmail.com>
2016-10-10 20:51:57 -0500

    Renamed HomeController to TaskController. Cleaned up the view.

--- ForgetTheMilk/ForgetTheMilk/Controllers/TaskController.cs
+++ ForgetTheMilk/ForgetTheMilk/Controllers/TaskController.cs
@@ -2,7 +2,7 @@
 
 namespace ForgetTheMilk.Controllers
 {
-    public class HomeController : Controller
+    public class TaskController : Controller
     {
         public ActionResult Index()
         {
