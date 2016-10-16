d7217b59b115a65aa7f1dc61e4c69f8545a71ef0
Jim Shaw <jimxshaw@gmail.com>
2016-10-16 12:19:24 -0500

    Automated test for success-failure checking.

--- ForgetTheMilk/ConsoleVerification/Program.cs
+++ ForgetTheMilk/ConsoleVerification/Program.cs
@@ -12,17 +12,35 @@ namespace ConsoleVerification
         {
             var input = "Buy groceries";
             var task = new Task(input);
+            var descriptionShouldBe = "Buy groceries";
+            DateTime? dueDateShouldBe = null;
 
-            Console.WriteLine($"Description: {task.Description}");
-            Console.WriteLine($"Due Date: {task.DueDate}");
-            Console.WriteLine();
+            if (descriptionShouldBe == task.Description && dueDateShouldBe == task.DueDate)
+            {
+                Console.WriteLine("SUCCESS");
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+                Console.WriteLine($"Description: {task.Description}");
+                Console.WriteLine($"Due Date: {task.DueDate}");
+            }
 
             input = "Buy groceries may 24";
             task = new Task(input);
+            descriptionShouldBe = "Buy groceries may 24";
+            dueDateShouldBe = new DateTime(2016, 5, 24);
 
-            Console.WriteLine($"Description: {task.Description}");
-            Console.WriteLine($"Due Date: {task.DueDate}");
-            Console.WriteLine();
+            if (descriptionShouldBe == task.Description && dueDateShouldBe == task.DueDate)
+            {
+                Console.WriteLine("SUCCESS");
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+                Console.WriteLine($"Description: {task.Description}");
+                Console.WriteLine($"Due Date: {task.DueDate}");
+            }
 
             Console.ReadLine();
         }
