1c48a493052abf8ed00e1fee11befedd287424a3
Jim Shaw <jimxshaw@gmail.com>
2016-10-16 11:23:05 -0500

    Refactored Add action code to Task constructor. Use console app to test TaskController add action.

--- ForgetTheMilk/ConsoleVerification/Program.cs
+++ ForgetTheMilk/ConsoleVerification/Program.cs
@@ -1,15 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ForgetTheMilk.Models;
+using System;
 
 namespace ConsoleVerification
 {
+    // The purpose of this console application project in our solution is for testing.
+    // If we can testing simple user input from a console app, it reduces the time and 
+    // hassle of repeatedly building, launching and refreshing our bulky web app.
     class Program
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Add a task: ");
+                var input = Console.ReadLine();
+                var task = new Task(input);
+
+                Console.WriteLine($"Description: {task.Description}");
+                Console.WriteLine($"Due Date: {task.DueDate}");
+                Console.WriteLine();
+            }
         }
     }
 }
--- ForgetTheMilk/ForgetTheMilk/Controllers/TaskController.cs
+++ ForgetTheMilk/ForgetTheMilk/Controllers/TaskController.cs
@@ -1,7 +1,5 @@
 ﻿using ForgetTheMilk.Models;
-using System;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using System.Web.Mvc;
 
 namespace ForgetTheMilk.Controllers
@@ -18,21 +16,7 @@ namespace ForgetTheMilk.Controllers
         [HttpPost]
         public ActionResult Add(string task)
         {
-            var taskItem = new Task()
-            {
-                Description = task
-            };
-
-            var dueDatePattern = new Regex(@"may\s(\d)");
-
-            var hasDueDate = dueDatePattern.IsMatch(task);
-
-            if (hasDueDate)
-            {
-                var dueDate = dueDatePattern.Match(task);
-                var day = Convert.ToInt32(dueDate.Groups[1].Value);
-                taskItem.DueDate = new DateTime(DateTime.Today.Year, 5, day);
-            }
+            var taskItem = new Task(task);
 
             Tasks.Add(taskItem);
 
