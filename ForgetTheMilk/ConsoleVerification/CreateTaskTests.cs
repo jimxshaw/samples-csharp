aeab0422cff9419de60e0dd2512e230bb9248f21
Jim Shaw <jimxshaw@gmail.com>
2016-10-19 19:23:23 -0500

    Ported over remaining unit tests from previous console app.

--- ForgetTheMilk/ConsoleVerification/CreateTaskTests.cs
+++ ForgetTheMilk/ConsoleVerification/CreateTaskTests.cs
@@ -16,5 +16,29 @@ namespace ConsoleVerification
             Expect(task.Description, Is.EqualTo(input));
             Assert.AreEqual(null, task.DueDate);
         }
+
+        [Test]
+        public void TestMonthDueDateWithYearWrap()
+        {
+            var input = "Buy groceries oct 13";
+
+            var today = new DateTime(2016, 10, 16);
+
+            var task = new Task(input, today);
+
+            Expect(task.DueDate, Is.EqualTo(new DateTime(2017, 10, 13)));
+        }
+
+        [Test]
+        public void TestMonthDueDateWithoutYearWrap()
+        {
+            var input = "Buy groceries oct 25";
+
+            var today = new DateTime(2016, 10, 16);
+
+            var task = new Task(input, today);
+
+            Expect(task.DueDate, Is.EqualTo(new DateTime(2016, 10, 25)));
+        }
     }
 }
