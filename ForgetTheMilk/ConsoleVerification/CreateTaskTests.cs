be3cee1694496df6102bc85dda989e7a80aeb8f4
Jim Shaw <jimxshaw@gmail.com>
2016-10-19 19:12:42 -0500

    Cleaned up unit test.

--- ForgetTheMilk/ConsoleVerification/CreateTaskTests.cs
+++ ForgetTheMilk/ConsoleVerification/CreateTaskTests.cs
@@ -10,21 +10,11 @@ namespace ConsoleVerification
         public void DescriptionAndNoDueDate()
         {
             var input = "Buy groceries";
-            Console.WriteLine($"Scenario 1: {input}");
 
             var task = new Task(input, default(DateTime));
 
-            var descriptionShouldBe = "Buy groceries";
-            DateTime? dueDateShouldBe = null;
-
-            var success = descriptionShouldBe == task.Description && dueDateShouldBe == task.DueDate;
-            var failureMessage = "ERROR: "
-                    + "\n"
-                    + $"Description: {task.Description} -> Should Be: {descriptionShouldBe}"
-                    + "\n"
-                    + $"Due Date: {task.DueDate} -> Should Be: {dueDateShouldBe}";
-
-            Assert.That(success, failureMessage);
+            Assert.AreEqual(input, task.Description);
+            Assert.AreEqual(null, task.DueDate);
         }
     }
 }
