c39428086a1096ec35a49c2d1deee11fe2917c76
Jim Shaw <jimxshaw@gmail.com>
2016-10-30 10:47:56 -0500

    Created LinkValidator class with method to validate links. Wrote tests to be used with LinkValidator.

---
+++ ForgetTheMilk/ConsoleVerification/LinkValidationTests.cs
@@ -0,0 +1,24 @@
+﻿using ForgetTheMilk.Controllers;
+using NUnit.Framework;
+
+namespace ConsoleVerification
+{
+    public class LinkValidationTests : AssertionHelper
+    {
+        [Test]
+        public void Validate_InvalidUrl_ThrowsException()
+        {
+            var invalidLink = "http://www.thisfakelinkdoesnotexist.org";
+
+            Expect(() => new LinkValidator().Validate(invalidLink), Throws.Exception.With.Message.EqualTo($"Invalid link {invalidLink}"));
+        }
+
+        [Test]
+        public void Validate_ValidUrl_DoesNotThrowException()
+        {
+            var validLink = "http://www.google.com";
+
+            Expect(() => new LinkValidator().Validate(validLink), Throws.Nothing);
+        }
+    }
+}
---
+++ ForgetTheMilk/ForgetTheMilk/Controllers/LinkValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+
+namespace ForgetTheMilk.Controllers
+{
+    public class LinkValidator
+    {
+        public void Validate(string link)
+        {
+            var request = WebRequest.CreateHttp(link);
+            request.Method = "HEAD";
+
+            try
+            {
+                request.GetResponse();
+            }
+            catch (WebException ex)
+            {
+                throw new ApplicationException($"Invalid link {link}");
+            }
+        }
+    }
+}
