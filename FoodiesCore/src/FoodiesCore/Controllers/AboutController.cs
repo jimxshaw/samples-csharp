39de69a5a90196c1dd15a0239ee32680e955a4f8
Jim Shaw <jimxshaw@gmail.com>
2016-11-12 18:04:18 -0600

    Tested attribute based routing on AboutController.

--- FoodiesCore/src/FoodiesCore/Controllers/AboutController.cs
+++ FoodiesCore/src/FoodiesCore/Controllers/AboutController.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FoodiesCore.Controllers
 {
+    [Route("[controller]")]
     public class AboutController
     {
+        [Route("")] // By not specifying a route explicitly, this will serve as the default action when hitting the about controller.
         public string Home()
         {
             return "1-555-555-5555";
         }
 
+        [Route("[action]")]
         public string Address()
         {
             return "USA";
