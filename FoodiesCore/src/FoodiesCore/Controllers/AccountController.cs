89853447d5463488f0009bc2ccad626d81268165
Jim Shaw <jimxshaw@gmail.com>
2016-12-25 18:01:45 -0600

    Updated view imports. Created boilerplate Register view.

---
+++ FoodiesCore/src/FoodiesCore/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoodiesCore.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodiesCore.Controllers
+{
+    public class AccountController : Controller 
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterUserViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                
+            }
+
+            return View();
+        }
+    }
+}
