e0dd88aab2d83939179558b8c3963f0551d7e409
Jim Shaw <jimxshaw@gmail.com>
2017-01-29 20:53:20 -0600

    Added Cars sample project to test out LINQ.

--- Foodies/OdeToFood/App_Start/AuthConfig.cs
+++ Foodies/OdeToFood/App_Start/AuthConfig.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Web.WebPages.OAuth;
-using OdeToFood.Models;
-
-namespace OdeToFood
+﻿namespace OdeToFood
 {
     public static class AuthConfig
     {
