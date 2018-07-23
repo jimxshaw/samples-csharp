387607d64f2eae2af391fcac719d559af40f4173
Jim Shaw <jimxshaw@gmail.com>
2018-07-22 19:28:53 -0500

    Add test class with test of Maximum method.

---
+++ UnitTests/01 AutomatedTests/Demo/Demo/MyArrayTests.cs
@@ -0,0 +1,33 @@
+﻿using Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+  public class MyArrayTests
+  {
+    // Method under test.
+    // Scenario that is tested.
+    // Expected behavior, state, result, etc.
+    public void Maximum_ArrayContainsOneValue_ReturnsThatValue()
+    {
+      // MyArray class's Maximum method called on an array that contains
+      // one value will return that value.
+
+      // Arrange.
+      var array = new MyArray(new[] { 4 });
+
+      // Act.
+      int max = array.Maximum();
+
+      // Assert.
+      if (max != 4)
+      {
+        throw new Exception();
+      }
+    }
+  }
+}
