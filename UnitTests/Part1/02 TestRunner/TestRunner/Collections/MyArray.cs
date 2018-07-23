cd71e776b154fec5d17e2a63397cad71a7872f97
Jim Shaw <jimxshaw@gmail.com>
2018-07-22 19:46:54 -0500

    Add boilerplate for TestRunner test project.

---
+++ UnitTests/Part1/02 TestRunner/TestRunner/Collections/MyArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+  public class MyArray
+  {
+    private int[] Data { get; }
+
+    public MyArray(IEnumerable<int> values)
+    {
+      Data = values.ToArray();
+    }
+
+    public int Maximum()
+    {
+      // Assume this.data.Length > 0
+      int max = Data[0];
+
+      for (var i = 1; i < Data.Length; i++)
+      {
+        if (Data[i] > max)
+        {
+          max = Data[i];
+        }
+      }
+
+      return max;
+    }
+
+  }
+}
