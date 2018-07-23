9110f7257de528da329e52157a1f5b48aaabe1b6
Jim Shaw <jimxshaw@gmail.com>
2018-07-22 20:18:23 -0500

    Add more tests for my array class.

--- UnitTests/Part1/02 TestRunner/TestRunner/CollectionsTests/MyArrayTests.cs
+++ UnitTests/Part1/02 TestRunner/TestRunner/CollectionsTests/MyArrayTests.cs
@@ -17,16 +17,52 @@ namespace Demo
       // MyArray class's Maximum method called on an array that contains
       // one value will return that value.
 
-      // Arrange.
-      var array = new MyArray(new[] { 4 });
+      // Arrange
+      MyArray array = new MyArray(new[] { 5 });
 
-      // Act.
+      // Act
       int max = array.Maximum();
 
-      // Assert.
-      if (max != 4)
+      // Assert
+      Assert(max == 5, "Single element not recognized as greatest.");
+    }
+
+
+    public void Maximum_GreatestElementFirst_ReturnsThatElement()
+    {
+      MyArray array = new MyArray(new[] { 5, 1, 2, 3, 4 });
+
+      int max = array.Maximum();
+
+      Assert(max == 5, "First element not recognized as greatest.");
+    }
+
+
+    public void Maximum_GreatestElementInTheMiddle_ReturnsThatElement()
+    {
+      MyArray array = new MyArray(new[] { 1, 2, 3, 5, 4 });
+
+      int max = array.Maximum();
+
+      Assert(max == 5, "Middle element not recognized as greatest.");
+    }
+
+
+    public void Maximum_GreatestElementLast_ReturnsThatElement()
+    {
+      MyArray array = new MyArray(new[] { 1, 2, 3, 4, 5 });
+
+      int max = array.Maximum();
+
+      Assert(max == 5, "Last element not recognized as greates.");
+    }
+
+
+    private static void Assert(bool condition, string message)
+    {
+      if (!condition)
       {
-        throw new Exception();
+        throw new Exception(message);
       }
     }
   }
