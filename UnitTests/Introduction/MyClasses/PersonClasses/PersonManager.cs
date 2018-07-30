facdc3b48f8ede5a22995080a11c7c74f57ee9b6
Jim Shaw <jimxshaw@gmail.com>
2018-07-29 20:05:49 -0500

    Add person classes.

---
+++ UnitTests/Introduction/MyClasses/PersonClasses/PersonManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClasses.PersonClasses
+{
+  public class PersonManager
+  {
+    public Person CreatePerson(string firstName, string lastName, bool isSupervisor)
+    {
+      Person result = null;
+
+      if (!string.IsNullOrEmpty(firstName))
+      {
+        if (isSupervisor)
+        {
+          result = new Supervisor();
+        }
+        else
+        {
+          result = new Employee();
+        }
+
+        result.FirstName = firstName;
+        result.LastName = lastName;
+      }
+
+      return result;
+    }
+  }
+}
