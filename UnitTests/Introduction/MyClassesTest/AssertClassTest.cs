d44c1e8d887bc42f3ec0f6b34232199529260ac3
Jim Shaw <jimxshaw@gmail.com>
2018-07-29 20:19:58 -0500

    Add tests for create person method.

---
+++ UnitTests/Introduction/MyClassesTest/AssertClassTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses.PersonClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassesTest
+{
+  [TestClass]
+  public class AssertClassTest
+  {
+    [TestMethod]
+    [Owner("Jefferson")]
+    public void IsInstanceOfTypeTest()
+    {
+      var manager = new PersonManager();
+      Person person;
+
+      person = manager.CreatePerson("John", "Duncan", true);
+
+      Assert.IsInstanceOfType(person, typeof(Supervisor));
+    }
+
+    [TestMethod]
+    [Owner("Jefferson")]
+    public void IsNullTest()
+    {
+      var manager = new PersonManager();
+      Person person;
+
+      person = manager.CreatePerson("", "Duncan", true);
+
+      Assert.IsNull(person);
+    }
+  }
+}
